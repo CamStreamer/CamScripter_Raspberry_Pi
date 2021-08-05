0d85c66d1dd516a6d73a6911345e26e165360d59
frantisek.dostal <frantisek.dostal@netrex.cz>
2021-08-05 17:01:43 +0200

    unregister deescalate

--- systemd_unregister.sh
+++ systemd_unregister.sh
@@ -3,6 +3,4 @@
 sudo systemctl stop camscripter
 sudo systemctl disable camscripter
 sudo rm /etc/systemd/system/camscripter.service
-systemctl daemon-reload
-systemctl reset-failed
 echo "CamScripter service is removed "
